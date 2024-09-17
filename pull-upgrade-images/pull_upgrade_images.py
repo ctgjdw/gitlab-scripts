a0c5a124731867eb45fd846624acbee4526b17f3
ctg <ctg@example.com>
2024-09-17 17:32:14 +0800

    feat: write docker images to a file for downloading

--- pull-upgrade-images/pull_upgrade_images.py
+++ pull-upgrade-images/pull_upgrade_images.py
@@ -1,10 +1,9 @@
 import sys
-from argparse import ArgumentParser
+from argparse import ArgumentParser, BooleanOptionalAction
 from logging import Logger, StreamHandler
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 import pyperclip  # To access the clipboard
 import time
 
@@ -13,23 +12,19 @@ LOGGER.addHandler(StreamHandler(sys.stdout))
 
 def get_upgrade_path_via_selenium(current_ver: str, gitlab_edition: str):
     LOGGER.info("Retrieving upgrade path from the Official Gitlab Upgrade Tool for Gitlab %s - %s", current_ver, gitlab_edition)
-    # Set up Chrome options for headless mode
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
 
-    # Initialize the WebDriver with headless options
-    driver = webdriver.Chrome(options=chrome_options)
+    # Initialize the WebDriver for Edge
+    driver = webdriver.Edge()
 
     try:
         # Open the URL
-        driver.get(f"https://gitlab-com.gitlab.io/support/toolbox/upgrade-path/?current={current_ver}&edition={gitlab_edition}")
+        driver.get(f"https://gitlab-com.gitlab.io/support/toolbox/upgrade-path/?current={current_ver}&distro=docker&edition={gitlab_edition}")
 
         # Wait for the page to fully load
         time.sleep(4)
 
-        # Find the "Copy to Clipboard" button by its CSS selector or XPath
-        copy_button = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[3]/button')
+        # Find the "Copy to Clipboard" button by its XPath
+        copy_button = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[3]/button')
 
         # Click the "Copy to Clipboard" button
         copy_button.click()
@@ -45,20 +40,30 @@ def get_upgrade_path_via_selenium(current_ver: str, gitlab_edition: str):
         # Close the browser
         driver.quit()
 
+def get_gitlab_image_tag(version: str, gitlab_edition: str) -> str:
+    return f"gitlab/gitlab-{gitlab_edition}:{version}-{gitlab_edition}.0"
+
 def main(args):
     upgrade_path = get_upgrade_path_via_selenium(args.current_version, args.gitlab_edition)
     versions = [v.strip() for v in upgrade_path.split("=>")]
-    final_version = versions[-1]
-    docker_images = [i for i in versions]
+    LOGGER.info("Upgrade path is: %s", versions)
+    
+    final_version = f"{versions[-1][0:-2]}.0"
+    docker_images = [f"{get_gitlab_image_tag(i, args.gitlab_edition)}\n" for i in versions]
+
+    if args.get_runner_image:
+        docker_images.append(f"gitlab/gitlab-runner:v{final_version}")
 
     if (output_path:=args.output_file):
+        LOGGER.info("Writing gitlab upgrade images to %s", output_path)
         with open(output_path, "w", encoding="utf-8") as file:
-            file.writelines()
+            file.writelines(docker_images)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="This CLI will pull and package the required docker images for upgrading Gitlab and it's runners.")
     parser.add_argument("current_version", help="The current version to upgrade from.")
     parser.add_argument("gitlab_edition", help="The gitlab edition. (ce/ee)", default="ce")
+    parser.add_argument("--get-runner-image", "-r", required=False, help="Gets the gitlab runner image as well.", default=False, action=BooleanOptionalAction)
     parser.add_argument("--output-file", "-o", required=False, help="Output the required docker images to pull into a text file, where the value is the file name")
     main(parser.parse_args())
